1b7d9839d0ced3f8001074fd22b6a2132681d1a7
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2023-11-06 15:49:24 +0100

    Add getAllWorkouts method in service and GetMapping in controller

--- backend/src/main/java/de/neuefische/team2/backend/controller/WorkoutController.java
+++ backend/src/main/java/de/neuefische/team2/backend/controller/WorkoutController.java
@@ -1,9 +1,13 @@
 package de.neuefische.team2.backend.controller;
 
+import de.neuefische.team2.backend.models.Workout;
 import de.neuefische.team2.backend.service.WorkoutService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -11,4 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkoutController {
     private WorkoutService workoutService;
 
+    @GetMapping("")
+    public List<Workout> getAllWorkouts() {
+        return workoutService.getAllWorkouts();
+    }
 }
