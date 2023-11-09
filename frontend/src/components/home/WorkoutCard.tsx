df711d36bc04f4ce7f41ac521bbcf3f3cada6670
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2023-11-09 13:14:19 +0100

    Enable onClick-event on WorkoutCard

--- frontend/src/components/home/WorkoutCard.tsx
+++ frontend/src/components/home/WorkoutCard.tsx
@@ -1,10 +1,12 @@
 import {TypeWorkoutCard} from "../../interfaces/types.ts";
 import "./WorkoutCard.css";
+import {useNavigate} from "react-router-dom";
 
 function WorkoutCard (props: TypeWorkoutCard) {
+    const navigate = useNavigate();
 
     return (
-        <section className={"workout-card"}>
+        <section onClick={() => navigate("/workout/" + props.workout.id)} className={"workout-card"}>
             <h3>{props.workout.workoutName}</h3>
             <p>{props.workout.plan}</p>
             <p>{props.workout.day}</p>
