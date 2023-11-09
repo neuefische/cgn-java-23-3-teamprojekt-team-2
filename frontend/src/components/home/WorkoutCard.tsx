3caede06869d2eb84bb36856d4f7570d7d6c0a77
Aljoscha Nyang <a.nyang@gmx.de>
2023-11-09 16:27:48 +0100

    Add route to details page, add keyboard accessibility and ARIA label to WorkoutCard component

--- frontend/src/components/home/WorkoutCard.tsx
+++ frontend/src/components/home/WorkoutCard.tsx
@@ -1,10 +1,30 @@
 import {TypeWorkoutCard} from "../../interfaces/types.ts";
 import "./WorkoutCard.css";
+import {useNavigate} from "react-router-dom";
+import React from "react";
 
 function WorkoutCard (props: TypeWorkoutCard) {
+    const navigate = useNavigate();
+
+    function handleClick():void {
+        navigate(`/workout/${props.workout.id}`);
+    }
+
+    function handleKeyDown(event:React.KeyboardEvent):void {
+        if(event.key === "Enter" || event.key === " ") {
+            navigate(`/workout/${props.workout.id}`);
+        }
+    }
 
     return (
-        <section className={"workout-card"}>
+        <section
+            role={"button"}
+            onClick={handleClick}
+            onKeyDown={handleKeyDown}
+            tabIndex={0}
+            aria-label={"Go to workout details"}
+            className={"workout-card"}
+        >
             <h3>{props.workout.workoutName}</h3>
             <p>{props.workout.plan}</p>
             <p>{props.workout.day}</p>
